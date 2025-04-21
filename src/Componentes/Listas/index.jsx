0978b4472f7582faeb21ea81e0dbe334695cb811
Daniel Caro <94130991+CaroDaniel@users.noreply.github.com>
2025-04-21 19:45:51 +0000

    main

--- src/Componentes/Listas/index.jsx
+++ src/Componentes/Listas/index.jsx
@@ -1,28 +1,56 @@
 import { useState, useEffect } from "react";
+import Filtro from '../Filtro';
 import './style.css'
 
 function Listas() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [tipoSeleccionado, setTipoSeleccionado] = useState('All');
+  const [busqueda, setBusqueda] = useState('');
 
-    useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon/?limit=1025")
-            .then(response => response.json())
-            .then(responseData => setData(responseData.results))
-            .catch(error => console.error("Error:", error));
-    }, []);
+  useEffect(() => {
+    const obtenerDatos = async () => {
+      if (tipoSeleccionado === 'All') {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1025`);
+        const json = await res.json();
+        setData(json.results);
+      } else {
+        const res = await fetch(`https://pokeapi.co/api/v2/type/${tipoSeleccionado}`);
+        const json = await res.json();
+        const listaFiltrada = json.pokemon.map(p => p.pokemon);
+        setData(listaFiltrada);
+      }
+    };
+    
+  
+    obtenerDatos();
+  }, [tipoSeleccionado]);
 
-    return (
-    <section className='c-lista'>
-      {data.map((pokemon, index) => (
-        <div className='c-lista-pokemon'
-        key={index}>
-          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.url.split("/")[6]}.png`} 
-                alt={`Pokémon ${pokemon.name}`} width='auto' height='60' loading='lazy'
-              />
-          <p>{pokemon.name}</p>
-        </div>
-      ))}
-    </section>
+  const handleTipoChange = (tipo) => {
+    setTipoSeleccionado(tipo);
+  };
+
+  return (
+    <>
+    <input
+        type="text"
+        placeholder="Buscar Pokémon"
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+        className="c-buscador"
+      />
+      <Filtro onTipoChange={handleTipoChange} />
+      <section className='c-lista'>
+        {data.map((pokemon, index) => (
+          <div className='c-lista-pokemon'
+            key={index}>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.url.split("/")[6]}.png`}
+              alt={`Pokémon ${pokemon.name}`} width='auto' height='60' loading='lazy'
+            />
+            <p>{pokemon.name}</p>
+          </div>
+        ))}
+      </section>
+    </>
   );
 }
 
